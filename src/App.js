352978a6b43e63f57f7d1b87820689ad0aaba0c1
Ryan Nicoletti <ryannicoletti@Ryans-MacBook-Pro-2.local>
2020-11-12 21:19:34 -0500

    added shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
     </Switch>
   );
 }
