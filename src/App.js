e8a4132831f954619c9262a3b3062e5ca4b77abf
Ryan Nicoletti <ryannicoletti@Ryans-MacBook-Pro-2.local>
2020-11-14 21:25:30 -0500

    added sign up form component and input component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/homepage/homepage";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
